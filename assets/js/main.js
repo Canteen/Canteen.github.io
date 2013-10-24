27f06c68d4d2d81ca06df8de653fcd9bf3e99b59
Matt Moore <matt@cloudkid.com>
2013-10-23 23:53:03 -0400

    Added more content, icons, theming

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,3 @@
 $(function(){
-	
-	// Create a new site
-	var site = new Canteen.Site();
-	
-	// Listen for when the site is ready
-	site.on('ready', function(){
-		Debug.log("The Canteen site is setup!");
-	});
-	
+		
 });
